d6985892097e58fe9f02f210aa034cee9b1ea3a1
LedesmaFran <fledesma@itba.edu.ar>
2022-08-06 19:40:50 -0300

    minor changes

--- functions.py
+++ functions.py
@@ -1,2 +1,9 @@
-def test():
-    print("hola")
+from importlib.resources import path
+from PIL import Image
+import numpy as np
+
+def openImage(path):
+    fs = Image.open(path)
+    print(fs.size)
+    display(fs)
+    return fs
