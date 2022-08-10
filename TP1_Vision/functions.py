d12ad2f8cb510fd272336ff9e83486155428e775
Pablo Smolkin <psmolkin@itba.edu.ar>
2022-08-10 15:35:40 -0300

    functions common y ej10ago

--- TP1_Vision/functions.py
+++ TP1_Vision/functions.py
@@ -7,27 +7,13 @@ from skimage.measure import block_reduce
 import numpy as np
 import math
 
-def displayImage(img, w, h):
-    display(img.resize((w,h), resample=Image.NEAREST))
-
 def makeSquare(sides_value, center_value):
     square = np.full(shape=(3,3), fill_value=sides_value, dtype=np.uint8)
     square[square.shape[0]//2,square.shape[1]//2] = center_value
     return square
 
-def tileImage(img, w_tile, h_tile):
-    w, h = img.size
-    im_mat = np.asarray(img)
-    im_tiled = im_mat.reshape(h//h_tile, h_tile, w//w_tile, w_tile)
-    im_tiled = im_tiled.swapaxes(1,2)
-    return im_tiled
-
-def pickPixelfromTile(img_tiled, pixel_pos):
-    return img_tiled[:,:,pixel_pos[0],pixel_pos[1]]
-
 def avgTiles(img_tiled):
     return img_tiled.mean(axis=(2,3)).astype(np.uint8)
-
 
 def getPixelSize(s_width, s_height, diagonal):
     factor = 0.0254 #m/inch
---
+++ common/functions.py
@@ -0,0 +1,22 @@
+from IPython.display import display
+from PIL import Image
+from skimage.measure import block_reduce
+import numpy as np
+
+def displayImage(img, w, h):
+    display(img.resize((w,h), resample=Image.NEAREST))
+
+def tileImage(img, w_tile, h_tile):
+    w, h = img.size
+    im_mat = np.asarray(img)
+    im_tiled = im_mat.reshape(h//h_tile, h_tile, w//w_tile, w_tile)
+    im_tiled = im_tiled.swapaxes(1,2)
+    return im_tiled
+
+def pickPixelfromTile(img_tiled, pixel_pos):
+    return img_tiled[:,:,pixel_pos[0],pixel_pos[1]]
+
+def downscaleImage(img, new_w, new_h):
+    mat = block_reduce(np.asarray(img), block_size=(img.size[0]//new_w, img.size[1]//new_h), func=lambda blocks, pixel_pos, axis: pickPixelfromTile(blocks, pixel_pos), func_kwargs={'pixel_pos': (0,0)})
+    img = Image.fromarray(mat)
+    return img
