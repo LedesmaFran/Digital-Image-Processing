ecf5d0a774e4d3c3b2f1bd37b039643121d1bc21
LedesmaFran <fledesma@itba.edu.ar>
2022-08-11 01:35:31 -0300

    ej3 casi terminado

--- TP1_Vision/functions.py
+++ TP1_Vision/functions.py
@@ -5,7 +5,6 @@ from IPython.display import display
 from PIL import Image
 from skimage.measure import block_reduce
 import numpy as np
-import math
 
 def makeSquare(sides_value, center_value):
     square = np.full(shape=(3,3), fill_value=sides_value, dtype=np.uint8)
@@ -15,8 +14,6 @@ def makeSquare(sides_value, center_value):
 def avgTiles(img_tiled):
     return img_tiled.mean(axis=(2,3)).astype(np.uint8)
 
-def getPixelSize(s_width, s_height, diagonal):
-    factor = 0.0254 #m/inch
-    degree = math.atan(s_height/s_width) #rad
-    pixel_side = diagonal*math.sin(degree)*factor/s_height #meter/pixel
-    print("lado pixel:",round(pixel_side*1e6,4),"µm")
+
+def getPixelSize(screen_density = 460):
+    return (print("Pixel size:", round(0.0254/screen_density*1e6,4), "µm"))
