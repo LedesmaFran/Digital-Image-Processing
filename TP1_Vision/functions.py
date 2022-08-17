2b016c2fd42d7122694e08a595931089f206a7f5
LedesmaFran <fledesma@itba.edu.ar>
2022-08-17 14:45:32 -0300

    commit tp2

--- TP1_Vision/functions.py
+++ TP1_Vision/functions.py
@@ -8,6 +8,25 @@ import numpy as np
 import math
 
 
+def displayImage(img, w, h):
+    display(img.resize((w,h), resample=Image.NEAREST))
+
+def tileImage(img, w_tile, h_tile):
+    w, h = img.size
+    im_mat = np.asarray(img)
+    im_tiled = im_mat.reshape(h//h_tile, h_tile, w//w_tile, w_tile)
+    im_tiled = im_tiled.swapaxes(1,2)
+    return im_tiled
+
+def pickPixelfromTile(img_tiled, pixel_pos):
+    return img_tiled[:,:,pixel_pos[0],pixel_pos[1]]
+
+def downscaleImage(img, new_w, new_h):
+    mat = block_reduce(np.asarray(img), block_size=(img.size[0]//new_w, img.size[1]//new_h), func=lambda blocks, pixel_pos, axis: pickPixelfromTile(blocks, pixel_pos), func_kwargs={'pixel_pos': (0,0)})
+    img = Image.fromarray(mat)
+    return img
+
+
 def makeSquare(sides_value, center_value):
     square = np.full(shape=(3,3), fill_value=sides_value, dtype=np.uint8)
     square[square.shape[0]//2,square.shape[1]//2] = center_value
