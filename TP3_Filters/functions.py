6adb67056cc5e1c7837e66046dda8ed95d6e1549
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2022-08-31 18:56:43 -0300

    Ej 6 - homomorphic filter hecho pero todavia no se ve como la imagen deseada

--- TP3_Filters/functions.py
+++ TP3_Filters/functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy import fft
+import matplotlib.pyplot as plt
 from cv2 import filter2D
 
 def lowpass_unit_kernel(img, n):
@@ -29,3 +31,24 @@ def HB_Filter(A,img):
     kernel = np.ones((n,n))*(-1)
     kernel[n//2,n//2] = A+8
     return filter2D(src=img, ddepth=-1, kernel=kernel)
+
+def homomorphic_filter(img, gamma_h=10, gamma_l=0.4, c=1, D0=0.1, plot_filter=False):
+    
+    m = np.log(img + 0.000001)
+    
+    M = fft.fft2(m)
+
+    d_center = (np.array(M.shape) - 1) / 2
+    d = np.array([np.sqrt((range(img.shape[0]) - d_center[0])**2 + (i - d_center[1])**2) for i in range(img.shape[1])]).T
+    
+    H = (gamma_h - gamma_l) * (1 - np.exp(-c * d**2 / (2*D0 * img.shape[0])**2)) + gamma_l
+    H = np.interp(H, (H.min(), H.max()), (H.min(), 1))
+
+    if (plot_filter):
+        plt.imshow(H)
+        plt.colorbar()
+
+    N = np.multiply(M, H)
+    n = np.exp(fft.ifft2(N, s=np.shape(img)).real)
+    
+    return n
