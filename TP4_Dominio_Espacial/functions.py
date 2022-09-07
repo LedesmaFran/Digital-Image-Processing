cb62bce8c64a142f6853fe30c21a43a2dd609683
Pablo Smolkin <psmolkin@itba.edu.ar>
2022-09-07 15:55:29 -0300

    Ej Gamma - TP4

--- TP4_Dominio_Espacial/functions.py
+++ TP4_Dominio_Espacial/functions.py
@@ -1,3 +1,6 @@
+import numpy as np
+from PIL import Image
+
 def imadjust(x,a,b,c,d):
     # Similar to imadjust in MATLAB.
     # Converts an image range from [a,b] to [c,d].
@@ -7,3 +10,27 @@ def imadjust(x,a,b,c,d):
     y[x_a] = c
     y[x_b] = d
     return y
+
+def build_gamma_comparison():
+    # Only for clarity in main code
+    desk = Image.open('img/GammaDesktop.png')
+    pablo = Image.open('img/GammaBlackW.png')
+    pablo_nm = Image.open('img/GammaBlackW_NM.png')
+    mati = Image.open('img/GammaMati.png')
+    mati_nm = Image.open('img/GammaMati_NM.png')
+    w, h = mati.size
+    desk = desk.resize((2*w,2*h), resample=Image.NEAREST)
+    pablo = pablo.resize((w,h), resample=Image.NEAREST)
+    pablo_nm = pablo_nm.resize((w,h), resample=Image.NEAREST)
+    mati_nm = mati_nm.resize((w,h), resample=Image.NEAREST)
+    desk = np.asarray(desk)
+    pablo = np.asarray(pablo)
+    pablo_nm = np.asarray(pablo_nm)
+    mati = np.asarray(mati)
+    mati_nm = np.asarray(mati_nm)
+
+    comp_H = np.concatenate((pablo, mati), axis=1)
+    comp_L = np.concatenate((pablo_nm, mati_nm), axis=1)
+    comp_sq = np.concatenate((comp_H, comp_L))
+    comp = np.concatenate((comp_sq, desk), axis=1)
+    return Image.fromarray(comp)
