abcc8472ff305a36dcbdc5a2f15a7a6fc1a44901
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2022-11-16 14:29:23 -0300

    GUI creacion de elipses completa

--- TP8_Tomografia/src/ellipse_item.py
+++ TP8_Tomografia/src/ellipse_item.py
@@ -10,10 +10,24 @@ class Ellipse():
         self.centro_x = centro_x
         self.centro_y = centro_y
         self.img = img
-        self.img_shape = np.shape(img)
+        b = self.norm_intensity()
+        self.color = (b, b, 0)
 
     def draw(self):
-        x_center = self.img_shape[0]*(self.centro_x+1)//2
-        y_center = self.img_shape[0]*(self.centro_y+1)//2
-        cv2.ellipse(Image=self.img, angle=self.inclinacion, center=(x_center, y_center), )
+        imag = cv2.imread(self.img)
+        self.img_shape = imag.shape
+        x_center = int(self.img_shape[0]*(self.centro_x+1)//2)
+        y_center = int(self.img_shape[1]*(self.centro_y+1)//2)
+        center = (x_center, y_center)
+
+        x_axis = int((self.img_shape[0]//2)*self.semi_eje_x)
+        y_axis = int((self.img_shape[1]//2)*self.semi_eje_y)
+        axes = (x_axis, y_axis)
+
+
+        return cv2.ellipse(imag, angle=self.inclinacion, center=center, startAngle=0, endAngle=360,
+                            axes=axes, color=self.color, thickness=-1)
+
+    def norm_intensity(self):
+        return int(255*(self.intensidad + 1)/2)
         
--- TP8_Tomografia/src/mainwindow.py
+++ TP8_Tomografia/src/mainwindow.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QMainWindow, QFileDialog, QInputDialog, QApplication
     QMessageBox
 from PyQt5.QtCore import QCoreApplication, QObject, QRunnable, QThread, QThreadPool, pyqtSignal, pyqtSlot
 from PyQt5 import uic, QtGui
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtOpenGL import *
 
 # Project modules
@@ -25,27 +25,51 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.ellipse_new.clicked.connect(self.draw_ellipse)
         self.ellipse_delete.clicked.connect(self.delete_ellipse)
 
-        self.ellipse_counter = 0
-
         self.ellipse_list.clicked.connect(self.update_counters)
 
         self.ellipses = []
 
-        self.img = np.zeros(self.img_box.size)
+        self.img = np.zeros((self.img_box.width(), self.img_box.height()))
+        self.im_path = 'image.png'
+        self.update_image()
+
+        self.nullRow = self.ellipse_list.currentRow()
+        
+        self.counter = 0
 
     def draw_ellipse(self):
-        self.ellipses.append(Ellipse())
-        return 0
+        self.ellipses.append(Ellipse(img=self.im_path, intensidad=self.intensidad_box.value(), 
+                                    inclinacion=self.inclinacion_box.value(), semi_eje_x=self.semi_eje_x_box.value(),
+                                    semi_eje_y=self.semi_eje_y_box.value(), centro_x=self.centro_x_box.value(), 
+                                    centro_y=self.centro_y_box.value()))
+        self.counter += 1
+        self.ellipse_list.addItem(f"Elipse {self.counter}")
+        self.update_image()
 
     def delete_ellipse(self):
-        # do stuff
-        return 0
+        if self.ellipse_list.currentRow() != self.nullRow:
+            self.ellipses.remove(self.ellipses[self.ellipse_list.currentRow()])
+            self.counter = self.counter - 1 if self.counter != 0 else 0
+            self.ellipse_list.takeItem(self.ellipse_list.currentRow())
+            for item in range(self.ellipse_list.count()):
+                self.ellipse_list.item(item).setText(f"Elipse {item + 1}")
+                
+            self.update_image()
     
     def update_counters(self):
-        item = self.ellipse_list.currentItem()
-
-        return 0
+        item = self.ellipse_list.currentRow()
+        el = self.ellipses[item]
+        self.intensidad_box.setValue(el.intensidad)
+        self.inclinacion_box.setValue(el.inclinacion)
+        self.semi_eje_x_box.setValue(el.semi_eje_x)
+        self.semi_eje_y_box.setValue(el.semi_eje_y)
+        self.centro_x_box.setValue(el.centro_x)
+        self.centro_y_box.setValue(el.centro_y)
 
     def update_image(self):
-        # do stuff
-        return 0
+        self.img = np.zeros((self.img_box.width(), self.img_box.height()))
+        cv2.imwrite(self.im_path, self.img)
+        for el in self.ellipses:
+            self.img = el.draw()
+            cv2.imwrite(self.im_path, self.img)
+        self.img_box.setPixmap(QPixmap(self.im_path))
