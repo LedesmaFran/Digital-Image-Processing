ff190a1a4b933d79d997db77823c16b50cb8fde8
LedesmaFran <fledesma@itba.edu.ar>
2023-01-18 21:08:23 -0300

    verilog files

---
+++ TPF_Wafer_Rotation/Verilog/dip2/dip2/src/tb_simulation.v
@@ -0,0 +1,69 @@
+`timescale 1ns/1ps 
+/**************************************************************************/
+/******************** Testbench for simulation ****************************/
+/**************************************************************************/
+// By FPGA4student.com
+`include "parameter.v"				// include definition file
+
+module tb_simulation;
+
+//-------------------------------------------------
+// Internal Signals
+//-------------------------------------------------
+
+reg HCLK, HRESETn;
+wire          vsync;
+wire          hsync;
+wire [ 7 : 0] data_R;
+wire [ 7 : 0] data_G;
+wire [ 7 : 0] data_B;
+wire enc_done;
+
+//-------------------------------------------------
+// Components
+//-------------------------------------------------
+
+image_read 
+#(.INFILE(`INPUTFILENAME))
+	u_image_read
+( 
+    .HCLK	                (HCLK    ),
+    .HRESETn	            (HRESETn ),
+    .VSYNC	                (vsync   ),
+    .HSYNC	                (hsync   ),
+    .DATA_R	            	(data_R ),
+    .DATA_G	            	(data_G ),
+    .DATA_B	            	(data_B ),
+	.ctrl_done				(enc_done)
+); 
+
+image_write 
+#(.INFILE(`OUTPUTFILENAME))
+	u_image_write
+(
+	.HCLK(HCLK),
+	.HRESETn(HRESETn),
+	.hsync(hsync),
+   .DATA_WRITE_R(data_R),
+   .DATA_WRITE_G(data_G),
+   .DATA_WRITE_B(data_B),
+
+	.Write_Done()
+);	
+
+//-------------------------------------------------
+// Test Vectors
+//-------------------------------------------------
+initial begin 
+    HCLK = 0;
+    forever #10 HCLK = ~HCLK;
+end
+
+initial begin
+    HRESETn     = 0;
+    #25 HRESETn = 1;
+end
+
+
+endmodule
+
